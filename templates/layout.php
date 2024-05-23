a6248c895723294bbfa7300bf9daed85adaa862f
arthurl7389 <161627821+arthurl7389@users.noreply.github.com>
2024-05-23 18:28:39 +0200

    Create layout.php

---
+++ templates/layout.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?= $titre ?></title>
+    </head>
+
+    <body class='site_wrapper'>
+        <p>COUCOU</p>
+    </body>
+</html>
