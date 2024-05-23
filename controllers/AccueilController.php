78567c8c392c7875b3074b2272df6c1311c7b559
arthurl7389 <161627821+arthurl7389@users.noreply.github.com>
2024-05-23 17:37:21 +0200

    Update and rename accueil.php to AccueilController.php

---
+++ controllers/AccueilController.php
@@ -0,0 +1,7 @@
+<?php
+class AccueilController {
+  public function index() {
+    //Charger la vue
+    require_once('templates/accueil.php');
+  }
+}
--- controllers/accueil.php
+++
@@ -1,6 +0,0 @@
-<?php $titre='Test Arthur'; ?>
-
-<?php ob_start(); ?>
-
-<?php ob_get_clean(); ?>
-<?php require('layout.php') ?>
