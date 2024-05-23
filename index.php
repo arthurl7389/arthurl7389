ed6b8f2f4e3f97c7161cab13deeb6c358050034c
arthurl7389 <161627821+arthurl7389@users.noreply.github.com>
2024-05-23 18:14:10 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//autoload pour charger automatiquement les classes
+spl_autoload_register(function ($class_name) {
+  include 'controllers/'.$class_name.'.php';
+});
+
+$page = isset($_GET['page']) ? $_GET['page'] : '';
+
+swith ($page) {
+  case 'accueil':
+  require_once('controllers/AccueilController.php');
+  $controller = new AccueilController();
+  $controller->index();
+  break;
